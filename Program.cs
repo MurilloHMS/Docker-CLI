ae9d0afa29243052bf7023d2d0582ed7881d19cb
MurilloHMS <Murillohenriquemelo@outlook.com>
2024-04-16 20:55:24 -0300

    incluido opção para selecionar o banco e vinculação de backups de clientes

--- Program.cs
+++ Program.cs
@@ -10,12 +10,14 @@ class Program
 {
     static async Task Main(string [] args)
     {
-
+        Console.Title = "ContainersArtsystem";
         var dockerClient = new DockerClientConfiguration(new Uri("npipe://./pipe/docker_engine")).CreateClient();
 
         while (true)
         {
             Console.Clear();
+            //Console.WriteLine("   _____            _        _                      \r\n  / ____|          | |      (_)                     \r\n | |     ___  _ __ | |_ __ _ _ _ __   ___ _ __ ___  \r\n | |    / _ \\| '_ \\| __/ _` | | '_ \\ / _ \\ '__/ __| \r\n | |___| (_) | | | | || (_| | | | | |  __/ |  \\__ \\ \r\n  \\_____\\___/|_| |_|\\__\\__,_|_|_| |_|\\___|_|  |___/ \r\n                                                    \r\n                                                    ");
+            Console.WriteLine("                _    _____           _                    _____        __ _                          \r\n     /\\        | |  / ____|         | |                  / ____|      / _| |                         \r\n    /  \\   _ __| |_| (___  _   _ ___| |_ ___ _ __ ___   | (___   ___ | |_| |___      ____ _ _ __ ___ \r\n   / /\\ \\ | '__| __|\\___ \\| | | / __| __/ _ \\ '_ ` _ \\   \\___ \\ / _ \\|  _| __\\ \\ /\\ / / _` | '__/ _ \\\r\n  / ____ \\| |  | |_ ____) | |_| \\__ \\ ||  __/ | | | | |  ____) | (_) | | | |_ \\ V  V / (_| | | |  __/\r\n /_/    \\_\\_|   \\__|_____/ \\__, |___/\\__\\___|_| |_| |_| |_____/ \\___/|_|  \\__| \\_/\\_/ \\__,_|_|  \\___|\r\n                            __/ |                                                                    \r\n                           |___/                                                                     ");
             //Criação do Menu
             Console.WriteLine("Selecione uma opção: ");
             Console.WriteLine("1. Criar container SQL Server");
@@ -34,7 +36,8 @@ class Program
                     string hostPort = Console.ReadLine();
                     Console.Write("Digite o nome do Container: ");
                     string containerName = Console.ReadLine();
-                    await CreateSqlServerContainerAsync(dockerClient,hostPort,containerName);
+                    string sqlVersion = RetornaVersao();
+                    await CreateSqlServerContainerAsync(dockerClient,hostPort,containerName, sqlVersion);
                     break;
                 case "2":
                     await ListAndInteractWithContainersAsync(dockerClient);
@@ -54,34 +57,97 @@ class Program
             }
 
         }
-    }    
+    }
 
-    static async Task CreateSqlServerContainerAsync(DockerClient dockerClient, string hostPort, string containerName )
+    private static string RetornaVersao()
+    {
+        //Menu de seleção da versão SQL
+        Console.WriteLine("Selecione uma versão do SQL:");
+        Console.WriteLine("1. SQL Server 2022");
+        Console.WriteLine("2. SQL Server 2019");
+        Console.WriteLine("3. SQL Server 2017");
+        Console.Write("Opção: ");
+        
+        //coleta opção selecionada
+        string interactionOption = Console.ReadLine();
+        
+        switch(interactionOption)
+        {
+            case "1":
+                return "server:2022-latest";
+            case "2":
+                return "server:2019-latest";
+            case "3":
+                return "server:2017-latest";
+
+        }
+        return "";
+    }
+
+    static async Task CreateSqlServerContainerAsync(DockerClient dockerClient, string hostPort, string containerName, string sqlVersion)
     {
         Console.Clear();
-        //Configuração dos dados
+
+        // Configuração dos dados
         string saPassword = "1s860t77r@";
         string acceptEula = "Y";
         string colation = "Latin1_General_CI_CA";
 
-        //Container Sql Server
-        var createParameters = new CreateContainerParameters
+        // Pasta do executável
+        string executablePath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+        // Caminho para a pasta Backup_clientes
+        string backupPath = Path.Combine(executablePath, "Backup_Clientes");
+
+        // Verifica se a pasta Backup_clientes existe, senão, a cria
+        if (!Directory.Exists(backupPath))
         {
-            Image = "mcr.microsoft.com/mssql/server:2019-latest",
-            Env = new[] { "ACCEPT_EULA=" + acceptEula, "SA_PASSWORD=" + saPassword, "MSSQL_COLLATION=" + colation },
-            HostConfig = new HostConfig
+            Directory.CreateDirectory(backupPath);
+        }
+
+        // Pasta com o nome do contêiner
+        string containerFolderPath = Path.Combine(backupPath, containerName);
+
+        // Verifica se a pasta com o nome do contêiner existe, senão, a cria
+        if (!Directory.Exists(containerFolderPath))
+        {
+            Directory.CreateDirectory(containerFolderPath);
+        }
+
+        try
+        {
+            // Container Sql Server
+            var createParameters = new CreateContainerParameters
             {
-                PortBindings = new Dictionary<string, IList<PortBinding>>
+                Image = $"mcr.microsoft.com/mssql/{sqlVersion}",
+                Env = new[] { "ACCEPT_EULA=" + acceptEula, "SA_PASSWORD=" + saPassword, "MSSQL_COLLATION=" + colation },
+                HostConfig = new HostConfig
                 {
-                    {"1433/tcp", new List<PortBinding> { new PortBinding{ HostPort = hostPort} } }
-                }
-            },
-            Name = containerName,
-            ExposedPorts = new Dictionary<string, EmptyStruct> { { "1433/tcp", default(EmptyStruct) } },
-        };
-        // cria o container com os parametros informados
-        var response = await dockerClient.Containers.CreateContainerAsync(createParameters);
-        Console.WriteLine("Container SQL Server criado com sucesso.");
+                    PortBindings = new Dictionary<string, IList<PortBinding>>
+                    {
+                        {"1433/tcp", new List<PortBinding> { new PortBinding{ HostPort = hostPort} } }
+                    },
+                    Mounts = new List<Mount>
+                    {
+                        new Mount
+                        {
+                            Type = "bind",
+                            Source = @containerFolderPath,
+                            Target = "/bkp"
+                        }
+                    }
+                },
+                Name = containerName,
+                ExposedPorts = new Dictionary<string, EmptyStruct> { { "1433/tcp", default(EmptyStruct) } },
+            };
+            // Cria o container com os parâmetros informados
+            var response = await dockerClient.Containers.CreateContainerAsync(createParameters);
+            Console.WriteLine($"Container SQL Server criado com sucesso.\n\nVersão: {sqlVersion}");
+            Console.ReadKey();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Erro ao criar o contêiner: " + ex.Message);
+        }
     }
 
     //Inicia todos os containers
@@ -119,6 +185,8 @@ class Program
     static async Task ListAndInteractWithContainersAsync(DockerClient dockerClient)
     {
         Console.Clear();
+        Console.WriteLine("   _____            _        _                      \r\n  / ____|          | |      (_)                     \r\n | |     ___  _ __ | |_ __ _ _ _ __   ___ _ __ ___  \r\n | |    / _ \\| '_ \\| __/ _` | | '_ \\ / _ \\ '__/ __| \r\n | |___| (_) | | | | || (_| | | | | |  __/ |  \\__ \\ \r\n  \\_____\\___/|_| |_|\\__\\__,_|_|_| |_|\\___|_|  |___/ \r\n                                                    \r\n                                                    ");
+
         // Listar containers
         var containers = await dockerClient.Containers.ListContainersAsync(new ContainersListParameters { All = true }); // inclui containers parados
 
